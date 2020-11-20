e28591444ffe1d05d4c8bf196b52d2629ebf1ae6
Gareth Roddy <gareth.roddy@hemdahl.com>
2020-11-20 13:26:12 +0000

    Simplify WintracFile.ts

--- src/WintracFile.ts
+++ src/WintracFile.ts
@@ -1,9 +1,8 @@
 
-import * as Papa from 'papaparse';
-import *  as  _ from 'lodash'; 
-
 // Don't use in bifrost...
 const { arrayToHex } = require("./utils")
+import * as Papa from 'papaparse';
+import *  as  _ from 'lodash'; 
 
 const sensorMap = require("./wintrac-sensormap.json");
 
@@ -13,8 +12,7 @@ enum EventTypes {
 }
 export class WintracFile 
 {
-    constructor(private fileBuffer: number[], private fileName) {
-    }
+    constructor(private fileBuffer: number[], private fileName) {}
 
     getDeviceTypeID() {
         return this.fileBuffer[723];
@@ -22,35 +20,23 @@ export class WintracFile
 
     arraySegmentEquals(arr, offset, bytes) {
         for(let i = 0; i < bytes.length; i++) {
-            if (arr[i+offset] !== bytes[i]) {
-                return false;
-            }
+            if (arr[i + offset] !== bytes[i]) return false;
         }
         return true;
-    }
-
-    // Records begin with [0x00, 0xff, 0x54]  or just 0xFF, 0x54?
-    getEventRecords() {
-        let records = this.findOffsets(this.fileBuffer, 0x00, 0xff, 0x54);
-        return records.map((ptr, i, array) => { 
-            return (i < array.length) ? this.decodeEventRecord(this.fileBuffer, ptr): null;
-        }).filter(_.isObject);
     }
 
     decodeEventRecord(buf, offset) {
         const eventType = buf[offset + 7];
         const record = { Time: this.decodeTimestamp(buf, offset + 3), Location: offset, Raw: arrayToHex(buf, offset, 48), Type: eventType, Sensors: [], SensorChanges: null };
         if (eventType === EventTypes.SensorManifest) {
-            let sensorCount = this.readUIntLE(buf, offset + 10, 2)
+            const sensorCount = this.readUIntLE(buf, offset + 10, 2)
             record.Sensors = _.range(sensorCount).reduce((acc, sensor, index) => { 
                 const mappedSensor = this.findSensor(buf, offset + 16 + index*4)
-                if (mappedSensor) {
-                    acc.push({ ...mappedSensor, Offset: index * 2 })
-                }
+                if (mappedSensor) acc.push({ ...mappedSensor, Offset: index * 2 })
                 return acc;
             }, [])
         } else if (eventType === EventTypes.SensorChange) {
-            const mappedSensor = this.findSensor(buf, offset + 10);// sensorLookup.find(s => (s.BlockID === buf[offset + 10]) && (s.DataID === buf[offset + 11]));
+            const mappedSensor = this.findSensor(buf, offset + 10);
             if (mappedSensor) {
                 record.SensorChanges = { [mappedSensor.Name] : this.decodeSensor(buf, offset + 12)};
             }
@@ -58,31 +44,15 @@ export class WintracFile
         return record;
     }
 
-    findOffsets(buf, ...bytes) {
-        let ptrs = [];
-        for(let i = 0; i < buf.length; i++) {
-            if (this.arraySegmentEquals(buf, i, bytes)) {
-                ptrs.push(i);
-            }
-        }
-        return ptrs;
-    }
-
     findFirstOffset(buf, ...bytes) {
         for(let i = 0; i < buf.length; i++) {
-            if (this.arraySegmentEquals(buf, i, bytes)) {
-                return i;
-            }
+            if (this.arraySegmentEquals(buf, i, bytes)) return i;
         }
         return null;
     }
 
     getFileSensorManifestOffset(buf) {
-        let offset = this.findFirstOffset(buf, 0xFF, 0x4B, 0x04, 0x02, 0x0B);
-        if (offset) {
-            offset += 5;
-        }
-        return offset;
+        return this.findFirstOffset(buf, 0xFF, 0x4B, 0x04, 0x02, 0x0B) + 5;
     }
 
     readUIntLE(array: number[], offset: number, count: number) {
@@ -96,112 +66,49 @@ export class WintracFile
 
     findSensor(buffer, sensorOffset) {
         const typeKey = '0x' + this.getDeviceTypeID().toString(16).toUpperCase();
-        const sensors = sensorMap[typeKey];
-        if (!sensors) {
-            return null;
-        }
-        const [ blockID, dataID ] = [ buffer[sensorOffset], buffer[sensorOffset + 1]];
-        return sensors.find(s => s.BlockID === blockID && s.DataID === dataID);    
-    }
-
-    /* This function has probably become redundant. */
-    getSensorList() {
-        let fileSensorOffset = this.getFileSensorManifestOffset(this.fileBuffer);
-        
-        const sensorCount = this.readUIntLE(this.fileBuffer, fileSensorOffset, 2);
-        fileSensorOffset += 2;
-        console.log("getSensorList: Raw sensors:", arrayToHex(this.fileBuffer, fileSensorOffset, sensorCount*2));
-        // FIXME: This is a hack, we get this in some files, but not others.
-        if (!this.findSensor(this.fileBuffer, fileSensorOffset)) {
-            fileSensorOffset += 2;
-        }
-        return _.range(sensorCount).reduce((acc, n) => {
-            const sensor = this.findSensor(this.fileBuffer, fileSensorOffset + 2*n);
-            if (sensor && sensor.Name.includes("Logger")) {
-                acc.push({ ...sensor, Offset: 2*n });
-            }
-            return acc;
-        }, []);   
-    }
-
-    // Records begin with [0xFF, 0x46, 0x61] 
-    getTemperatureRecords(buf, sensors) {
-        let records = this.findOffsets(buf, 0xFF, 0x46, 0x61);
-        return records.map((ptr, i, array) => { 
-            return (i < array.length) ? this.decodeRecord(buf, ptr, sensors, array[i+1] - ptr): null;
-        }).filter(_.isObject);
+        const sensors = sensorMap[typeKey] || [];
+        return sensors.find(s => s.BlockID === buffer[sensorOffset] && s.DataID === buffer[sensorOffset + 1]);    
     }
 
     decodeRecord(buf, offset, sensors, length, sensorTemplate = {}) {
         let record = { Time: this.decodeTimestamp(buf, offset + 3), /*location: offset, raw: arrayToHex(buf, offset, 40),*/...sensorTemplate };
-        return sensors.reduce((map, sensor) => { 
-            map[sensor.Name] = this.decodeSensor(buf, 9 + offset + sensor.Offset);
-            return map;
-        }, record);
+        return sensors.reduce((map, sensor) => ({ ...map, [sensor.Name]: this.decodeSensor(buf, 9 + offset + sensor.Offset) }), record);
     }
 
-    // Timestamps are stored as the number of minutes since the unix epoch (1970-1-1) as a 32-bit unsigned value.
     decodeTimestamp(buf, offset) {
-        const minutesSinceEpoch = this.readUIntLE(buf, offset, 4);
-        return new Date(minutesSinceEpoch * 60 * 1000);
+        return new Date(this.readUIntLE(buf, offset, 4) * 60 * 1000);  // Timestamps are stored as the number of minutes since the unix epoch (1970-1-1) as a 32-bit unsigned value.
     }
 
-    // Most sensors are 16-bit signed integers
     decodeSensor(record, offset) {
-        const rawValue = this.readIntLE(record, offset, 2);
+        const rawValue = this.readIntLE(record, offset, 2); // Most sensors are 16-bit signed integers
         return (_.isFinite(rawValue) && rawValue !== 0x7FFF && rawValue !== 0x13FF) ? _.round(rawValue / 32.0, 1): null;
-    }
-    /**
-     * @deprecated
-    */ 
-    getRecordsDeprecated() {
-        const sensors = this.getSensorList();
-        const records = this.getTemperatureRecords(this.fileBuffer, sensors).filter(record => this.defaultRecordFilter(record));
-        return records;
     }
 
     get recordPrefixes() {
-        return { 
-            event: [0x00, 0xff, 0x54],
-            temperature: [0xff, 0x46, 0x61],
-        };
+        return { event: [0x00, 0xff, 0x54], temperature: [0xff, 0x46, 0x61] };
     }
 
     getRecords() {
-        let events = [];
-        let temperatures = [];
-        let sensors = [];
-        let sensorTemplate = {};
+        const res = { events: [], temperatures: [], sensors: [], sensorTemplate: {} };
         for(let ptr = 0; ptr < this.fileBuffer.length; ptr ++) {
             if (this.arraySegmentEquals(this.fileBuffer, ptr, this.recordPrefixes.event)) {
                 let ev = this.decodeEventRecord(this.fileBuffer, ptr);
-                if (ev.Type === EventTypes.SensorManifest) sensors = ev.Sensors; // Could just if (ev.sensors.length)
-                if (ev.Type === EventTypes.SensorChange && (ev.SensorChanges) )  {
-                    sensorTemplate = { ...sensorTemplate, ...ev.SensorChanges };
-                }
-                events.push(ev);
+                if (ev.Type === EventTypes.SensorManifest) res.sensors = ev.Sensors; // Could just if (ev.sensors.length)
+                if (ev.Type === EventTypes.SensorChange && (ev.SensorChanges) ) res.sensorTemplate = { ...res.sensorTemplate, ...ev.SensorChanges }
+                res.events.push(ev);
             } else if (this.arraySegmentEquals(this.fileBuffer, ptr, this.recordPrefixes.temperature)) { 
-                temperatures.push(this.decodeRecord(this.fileBuffer, ptr, sensors, 100, sensorTemplate));
+                res.temperatures.push(this.decodeRecord(this.fileBuffer, ptr, res.sensors, 100, res.sensorTemplate));
             }
         }
-        return _.orderBy(temperatures.filter(record => this.defaultRecordFilter(record)), t => t.Time.getTime());
+        return _.orderBy(res.temperatures.filter(record => this.defaultRecordFilter(record)), t => t.Time.getTime());
     }
 
     toCsv() {
-        const records = this.getRecords();
-        return Papa.unparse(records);
+        return Papa.unparse(this.getRecords());
     }
 
     defaultRecordFilter(record) {
-        if (!record.Time || record.Time.getTime() < 978307200000 /* 2000-01-01 */) { 
-            return false;
-        }
-        // We see some records like: 2008-10-10T00:45:00.000Z,1.7,0,0,0,0,0. These are not valid.
-        const values = Object.values(record);
-        if (values.filter(r => r).length <= 2) { 
-            return false;
-        }
-        return true;
+        return (record.Time && record.Time.getTime() >= 978307200000 /* 2000-01-01 */ && Object.values(record).filter(r => r).length > 2);
     }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const WintracFile = require("./dist/WintracFile");
-const _ = require("lodash");
+const { take } = require("lodash");
 
 
 async function testWTF(wtfFilePath, csvPath) {
@@ -17,17 +17,17 @@ async function testWTF(wtfFilePath, csvPath) {
     console.log("Event record count:", eventRecords.length, eventRecords[21]);
   //  console.log("Event 1:", take(eventRecords.filter(r => r.Time.8toISOString() >= '2001-06-15T15:29:00'), 10));
     let records = wtf.getRecords();
-    console.log("Event Records (sensor manifest):", _.take(eventRecords.filter(r => r.Time >= new Date('2016-06-14T10:25:00Z')).filter(r => r.Type === 0x45),20) );
+    console.log("Event Records (sensor manifest):", take(eventRecords.filter(r => r.Time >= new Date('2016-06-14T10:25:00Z')).filter(r => r.Type === 0x45),20) );
     console.timeEnd("parse-wtf");
     console.log("testWTF: DeviceId:", "0x" + wtf.getDeviceTypeID().toString(16));
     console.log("testWTF: Sensors:", wtf.getSensorList());
-    console.log("testWTF: Records:", _.take(records.filter(r => r.Time >= new Date('2016-06-14T17:44:00Z')), 5));//9/6/2016 12:41
+    console.log("testWTF: Records:", take(records.filter(r => r.Time >= new Date('2016-06-14T17:44:00Z')), 5));//9/6/2016 12:41
     //console.log("testWTF: CSV:", wtf.toCsv());
     fs.writeFileSync(csvPath, wtf.toCsv())
 }
 
 //testWTF("../sample_files/00187--1.wtf", "../output_files/00187.csv");
-testWTF(__dirname + "/../sample_files/MT577--1.wtf", __dirname + "/../output_files/MT577.csv");
+testWTF(__dirname + "/../sample_files/23853--1.wtf", __dirname + "/../output_files/23853.csv");
 //testWTF("../sample_files/CU1171--1.wtf", "../output_files/CU1171.csv");
 //testWTF("../sample_files/R4038--1.wtf", __dirname + "/../output_files/R4038.csv");
 //testWTF(path.join(__dirname, "../sample_files/110SP--2.wtf"), path.join(__dirname, "../output_files/110SP--2.csv"));
